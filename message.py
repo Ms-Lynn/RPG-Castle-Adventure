1643a9daffe7d1c5170499ffc6d7db88efba7534
Ms-Lynn <117663038+Ms-Lynn@users.noreply.github.com>
2023-04-26 19:40:45 +0000

    Version 2 saved

--- message.py
+++
@@ -1,3 +0,0 @@
-# Messages that will be reused through the game in a dictionary
-messages = { "Quit" : "Thank for playing!'\n'You have now quit the game. ",
-             "Error" : "Invalid choice, please make another selection. "}
